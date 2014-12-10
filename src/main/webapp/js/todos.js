5032e28e05617c868604ee2dc3ddd91910d0defc
Stefano Sanfilippo <a.little.coder@gmail.com>
2014-12-10 01:36:00 +0100

    Frontend.

---
+++ src/main/webapp/js/todos.js
@@ -0,0 +1,28 @@
+var tasksApp = angular.module("TasksApp", []);
+
+tasksApp.controller("TasksController", function ($scope, $http, $log) {
+    $http.get("api/todos/all").success(function(data) {
+        $scope.tasks = data;
+    });
+    
+    $scope.text = "";
+    $scope.body = "";
+
+    $scope.create = function() {
+        var data = {
+            title: $scope.title,
+            body: $scope.body
+        };
+
+        $log.info("Sending", data, "to server");
+
+        $http.post("api/todos/new", data)
+        .success(function(data) {
+            $scope.tasks.push(data);
+            $("#newtaskdialog").modal("hide");
+        })
+        .error(function(data) {
+            
+        });
+    }
+});
